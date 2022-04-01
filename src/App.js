7a3235633fb2c577650b545e5e971c39eb86c99e
Jake Pople <jakepople@hotmail.co.uk>
2022-04-01 15:42:23 +0100

    google map react added

--- src/App.js
+++ src/App.js
@@ -1,22 +1,53 @@
 // import logo from './logo.svg';
 import './App.css';
+import React, { Component } from 'react';
 import Flat from './components/flat';
 
-function App() {
-  const flat = {
-    "id": 145,
-    "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
-    "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
-    "price": 164,
-    "priceCurrency": "EUR",
-    "lat": 48.884211,
-    "lng": 2.346890
-  };
-  return (
-    <div>
-      <Flat flat={flat} />
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      flats: []
+    };
+  }
+
+  componentDidMount() {
+    const url = "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/flats.json";
+    fetch(url)
+      .then(response => response.json())
+      .then((data) => {
+        this.setState({
+          flats: data
+        })
+      })
+  }
+
+  // const flat = {
+  //   "id": 145,
+  //   "name": "Charm at the Steps of the Sacre Coeur/Montmartre",
+  //   "imageUrl": "https://raw.githubusercontent.com/lewagon/flats-boilerplate/master/images/flat1.jpg",
+  //   "price": 164,
+  //   "priceCurrency": "EUR",
+  //   "lat": 48.884211,
+  //   "lng": 2.346890
+  // };
+
+  // const flats = [ flat, flat, flat ];
+  render() {
+    return (
+      <div className="app">
+        <div className="main">
+          <div className="search"></div>
+          <div className="flats">
+            {this.state.flats.map((flat) => {
+              return <Flat flat={flat} />
+            })}
+          </div>
+        </div>
+        <div className="map"></div>
+      </div>
+    );
+  }
 }
 
 export default App;
