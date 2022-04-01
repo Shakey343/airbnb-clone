7956f0e746952a47b1bdaa23dc18f6c960c50ccf
Jake Pople <jakepople@hotmail.co.uk>
2022-04-01 14:29:21 +0100

    first component created

--- src/index.js
+++ src/index.js
@@ -3,14 +3,20 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Hello from './components/hello';
+// import Hello from './components/hello';
+
+// const root = <div>
+//   <Hello firstName="Jake" lastName="Pople"/>
+// </div>
 
 ReactDOM.render(
-  <React.StrictMode>
-    <Hello />
-  </React.StrictMode>,
+  <App />,
   document.getElementById('root')
 );
+
+//<React.StrictMode>
+//  <Hello name="Jake" />
+//</React.StrictMode>
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
